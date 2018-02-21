921815bad3aa992e4433a6360c5e71f92ba48025
Samuel Gasper <1516828@my.brunel.ac.uk>
2018-02-21 17:52:24 +0000

    Done the cards on the list page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+4.times do
+  flat = Flat.new(
+    name: Faker::OnePiece.island,
+    address: Faker::OnePiece.sea,
+    description: Faker::BackToTheFuture.quote,
+    price_per_night: (30..300).to_a.sample,
+    number_of_guests: (1..4).to_a.sample
+    )
+  flat.save
+end
